95c29f70b00db3988727f0d550a2c7a848fb3674
Thorn731 <notdefined@list.ru>
2012-12-11 17:16:38 +0400

    changed from pg to sqlite, added fields for omniauth to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121210153502) do
+ActiveRecord::Schema.define(:version => 20121211131348) do
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -26,6 +26,9 @@ ActiveRecord::Schema.define(:version => 20121210153502) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "uid"
+    t.string   "provider"
+    t.string   "access_token"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
