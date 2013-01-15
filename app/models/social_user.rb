cf23b09501bd84fc3a67eb193a894b21d08a0f6d
Thorn731 <notdefined@list.ru>
2013-01-15 16:14:12 +0400

    polished omniauth login features

--- app/models/social_user.rb
+++ app/models/social_user.rb
@@ -3,16 +3,41 @@ class SocialUser < ActiveRecord::Base
   attr_accessible :email, :access_token, :uid, :provider, :user_id
 
   def self.from_omniauth(auth, current_user)
-    where(auth.slice(:provider, :uid)).first_or_create do |user|
-      user.provider = auth.provider
-      user.uid = auth.uid
-      user.email = auth.info.email
-      user.access_token = auth.credentials.token
-      user.user_id = current_user.id
-      if ["twitter"].include?(auth.provider) and auth.info.email.nil?
-        user.email = "#{auth.info.nickname}@#{auth.provider}.com"
-      end
+    user_info = UserInfo.new(auth)
+    if user = find_by_email_and_provider(user_info.email, user_info.provider)
+      user.update_attributes(access_token: user_info.access_token)
+    else
+      create_with(user_info, current_user)
     end
   end
 
+  def self.create_with(info, current_user)
+    create(
+      provider: info.provider,
+      uid:      info.uid,
+      email:    info.email,
+      user_id:  current_user.id,
+      access_token: info.access_token
+    )
+  end
+
+end
+
+class UserInfo
+  def initialize(auth)
+    @auth = auth
+    @uid          = auth.uid
+    @access_token = auth.credentials.token
+    @provider     = auth.provider
+  end
+  attr_reader :uid, :access_token, :provider
+
+  def email
+    puts @auth.to_yaml
+    if @auth.info.email.nil?
+      "#{@auth.info.nickname}@#{@auth.provider}.com"
+    else
+      @auth.info.email
+    end
+  end
 end
