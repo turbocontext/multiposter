b8ac3496aa461b73f61bdc6340c648e4c973d9e8
Thorn731 <notdefined@list.ru>
2013-01-25 17:02:20 +0400

    more tests

--- spec/models/social_user_spec.rb
+++ spec/models/social_user_spec.rb
@@ -10,32 +10,6 @@ describe SocialUser do
       user_id: "12",
       email: "email@email.com"
     }
-  end
-  it "should create new social user with valid attributes" do
-    SocialUser.create(@attr).should be_true
-  end
-
-  describe "validations" do
-    it "should reject records without provider" do
-      SocialUser.new(@attr.merge(provider: nil)).should_not be_valid
-    end
-
-    it "should reject records without access token" do
-      SocialUser.new(@attr.merge(access_token: nil)).should_not be_valid
-    end
-  end
-
-  describe "relations" do
-    it "should have many message sets and messages through it" do
-      SocialUser.new.should respond_to(:message_sets)
-      SocialUser.new.should respond_to(:messages)
-    end
-  end
-
-end
-
-describe UserInfo do
-  before(:each) do
     @oauth1 = {
       provider: 'facebook',
       uid: '100003700047553',
@@ -120,10 +94,35 @@ describe UserInfo do
       }
     }
   end
+  it "should create new social user with valid attributes" do
+    SocialUser.create(@attr).should be_true
+  end
+
+  describe "validations" do
+    it "should reject records without provider" do
+      SocialUser.new(@attr.merge(provider: nil)).should_not be_valid
+    end
+
+    it "should reject records without access token" do
+      SocialUser.new(@attr.merge(access_token: nil)).should_not be_valid
+    end
+  end
+
+  describe "relations" do
+    it "should have many message sets and messages through it" do
+      SocialUser.new.should respond_to(:message_sets)
+      SocialUser.new.should respond_to(:messages)
+    end
+  end
+
   it "should extract user info from omniauth hash" do
     user_info = UserInfo.new(@oauth1)
     user_info.email.should == @oauth1[:info][:email]
     user_info = UserInfo.new(@oauth2)
     user_info.email.should == "offical.kavigator@twitter.com"
+  end
+
+  it "should create new user from omniauth" do
+    expect {SocialUser.from_omniauth(@oauth1)}.to change{SocialUser.count}.by(1)
   end
 end
