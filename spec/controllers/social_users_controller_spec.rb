375d8858df592672b8dd0f2ccdb0e81dcdcfb597
Thorn731 <notdefined@list.ru>
2013-01-26 15:21:31 +0400

    added tests for soical users controller

--- spec/controllers/social_users_controller_spec.rb
+++ spec/controllers/social_users_controller_spec.rb
@@ -1,1 +1,16 @@
 require "spec_helper"
+
+describe SocialUsersController do
+  before(:each) do
+      user = FactoryGirl.create(:user)
+      sign_in user
+  end
+
+  describe "GET index" do
+    it "should fetch all social users and return ok" do
+      get :index
+      assigns(:users).should == SocialUser.scoped
+      response.should be_ok
+    end
+  end
+end
