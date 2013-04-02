94d13c47d95f6cfc051aa6cbfa8adc4d4530c1d6
Thorn731 <notdefined@list.ru>
2013-04-02 12:42:20 +0400

    scenario for messaging

--- features/step_definitions/social_user_steps.rb
+++ features/step_definitions/social_user_steps.rb
@@ -10,10 +10,20 @@ Given(/^I create message$/) do
       check("model_ids_")
     end
   end
-  page.driver.save_screenshot Rails.root.join("tmp/capybara/#{Time.now.strftime('%Y-%m-%d-%H-%M-%S')}.png")
-  click_on "Create message"
+  within "#create_message_button" do
+    click_on "Create message"
+  end
+  fill_in "All facebook profiles", with: "Facebook profile message"
+  fill_in "All facebook pages", with: "Facebook pages message"
+  fill_in "All twitter profiles", with: "Twitter profile message"
+  click_on "Create"
+  # page.driver.save_screenshot Rails.root.join("tmp/capybara/#{Time.now.strftime('%Y-%m-%d-%H-%M-%S')}.png")
 end
 
 Then(/^messages should be created$/) do
-  pending # express the regexp above with the code you wish you had
+  click_on "Messages"
+  page.should have_content("Facebook profile message")
+  page.should have_content("Facebook page message")
+  page.should have_content("Twitter profile message")
+
 end
