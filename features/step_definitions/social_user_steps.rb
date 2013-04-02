5d1c13c5eef325418c65be5e3e703b32850587c7
Thorn731 <notdefined@list.ru>
2013-04-02 17:40:21 +0400

    fixed test with capybara not filling in all ids

--- features/step_definitions/social_user_steps.rb
+++ features/step_definitions/social_user_steps.rb
@@ -13,9 +13,13 @@ Given(/^I create message$/) do
   within "#create_message_button" do
     click_on "Create message"
   end
-  page.driver.save_screenshot Rails.root.join("tmp/capybara/#{Time.now.strftime('%Y-%m-%d-%H-%M-%S')}.png")
 
   fill_in "Content for all Test provider profiles", with: "Test provider profile message"
+  all('.test_provider_message textarea').each do |input|
+    fill_in(input[:id], with: "Test provider message")
+  end
+
+  # page.driver.save_screenshot Rails.root.join("tmp/capybara/#{Time.now.strftime('%Y-%m-%d-%H-%M-%S')}.png")
   click_on "Post message"
 end
 
