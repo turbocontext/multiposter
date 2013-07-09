43459200552d34397df472ca7bf33fc96948cb7a
thorn <notdefined@list.ru>
2013-07-09 13:22:56 +0400

    added localizations

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,28 @@
 TemplateApp::Application.routes.draw do
-  devise_for :users
+  scope "(:locale)", locale: /en|ru/ do
+    devise_for :users
 
-  match "/auth/:provider/callback" => "social_users#create"
+    match "/auth/:provider/callback" => "social_users#create"
 
-  resources :social_users do
-    collection do
-      post :mass_destroy
-      get :vkontakte
+    resources :social_users do
+      collection do
+        post :mass_destroy
+        get :vkontakte
+      end
+    end
+
+    # post '/message_sets/new'
+    resources :message_sets
+    resources :messages
+    resources :users
+
+    namespace :api, defaults: { format: 'json' } do
+      namespace :v1 do
+        resources :message_sets, only: :create
+        resources :messages, only: :destroy
+      end
     end
   end
-
-  # post '/message_sets/new'
-  resources :message_sets
-  resources :messages
-  resources :users
-
-  namespace :api, defaults: { format: 'json' } do
-    namespace :v1 do
-      resources :message_sets, only: :create
-      resources :messages, only: :destroy
-    end
-  end
-
   # root to: "social_users#index"
   root to: "message_sets#new"
 end
