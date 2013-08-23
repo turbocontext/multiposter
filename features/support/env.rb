a989aec37c7e9547c4ab03a7220f0bf279cefc5a
arsen netdev plugdev sound <arsen@arsen-pc.net>
2013-08-23 11:47:27 +0400

    changed env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -9,12 +9,28 @@ Spork.prefork do
     require 'simplecov'
     SimpleCov.start 'rails'
   end
+  # Loading more in this block will cause your tests to run faster. However,
+  # if you change any configuration or code from libraries loaded here, you'll
+  # need to restart spork for it take effect.
 
   require 'cucumber/rails'
 
   Capybara.default_selector = :css
-  # Capybara.server_port = 57124
+
   Capybara.javascript_driver = :webkit
+
+  ActionController::Base.allow_rescue = false
+
+  # Remove/comment out the lines below if your app doesn't have a database.
+  # For some databases (like MongoDB and CouchDB) you may need to use :truncation instead.
+  begin
+    DatabaseCleaner.strategy = :transaction
+  rescue NameError
+    raise "You need to add database_cleaner to your Gemfile (in the :test group) if you wish to use it."
+  end
+
+  Cucumber::Rails::Database.javascript_strategy = :truncation
+
 end
 
 Spork.each_run do
@@ -22,15 +38,8 @@ Spork.each_run do
     require 'simplecov'
     SimpleCov.start 'rails'
   end
-
-  ActionController::Base.allow_rescue = false
-  begin
-    DatabaseCleaner.strategy = :transaction
-  rescue NameError
-    raise "You need to add database_cleaner to your Gemfile (in the :test group) if you wish to use it."
-  end
-
-  # Cucumber::Rails::Database.javascript_strategy = :truncation
+  # This code will be run each time you run your specs.
 
 end
+
 
